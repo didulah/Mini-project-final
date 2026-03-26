8ba5bd71d0d0f7c0fedc5bbd4430b77dec059311
didulah <himasaragunaweera@gmail.com>
2026-03-27 01:35:56 +0530

    first commit

---
+++ project.sql
@@ -0,0 +1,28 @@
+-- CREATE TABLE student(
+-- stId integer(6) NOT NULL PRIMARY KEY,
+-- stName varchar(20) NOT NULL);
+
+-- insert into student values(249000,'Student 1');
+
+-- select*from student;
+
+-- CREATE TABLE lecture(
+-- lecId integer(4) NOT NULL PRIMARY KEY,
+-- lecDate date NOT NULL,
+-- startTime time NOT NULL,
+-- endTime time NOT NULL);
+
+-- insert into lecture values(
+-- 1111,'2025-11-03','08:30','10:30');
+
+-- select*from lecture;
+
+-- CREATE TABLE attendence(
+-- stId integer(6) NOT NULL,
+-- lecId integer(4) NOT NULL,
+-- attendedTime time NULL,
+-- status char(6) NULL);
+
+-- insert into attendence values(249000,1111,'','');
+
+-- select*from attendence;
